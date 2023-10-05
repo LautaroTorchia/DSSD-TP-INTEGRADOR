b2dc55d570734420d1c5d55ce1b4477a414a708f
Joaquin Olmos <joaquiniolmos@gmail.com>
2023-10-05 18:45:00 -0300

    login html slight change

--- dssd-furniture-frontend/src/components/login.js
+++ dssd-furniture-frontend/src/components/login.js
@@ -21,34 +21,36 @@ export default function LoginForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="container mt-5"> {/* Agrega la clase 'container' de Bootstrap */}
-      <div className="mb-3"> {/* Agrega la clase 'mb-3' para el margen inferior */}
-        <label htmlFor="username" className="form-label">Username:</label> {/* Agrega la clase 'form-label' */}
-        <input
-          type="text"
-          id="username"
-          className="form-control" 
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div className="mb-3"> {/* Agrega la clase 'mb-3' para el margen inferior */}
-        <label htmlFor="password" className="form-label">Password:</label> {/* Agrega la clase 'form-label' */}
-        <input
-          type="password"
-          id="password"
-          className="form-control" 
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <button type="submit" className="btn btn-primary">Login</button> {/* Agrega las clases 'btn' y 'btn-primary' */}
-      </div>
-      {error && <p className="error mt-3">{error}</p>} {/* Agrega la clase 'mt-3' para el margen superior */}
-    </form>
+    <div className="container mt-5 d-flex justify-content-center"> {/* Centra el contenido horizontalmente */}
+      <form onSubmit={handleSubmit} className="w-50"> {/* Establece un ancho máximo */}
+        <div className="mb-3"> {/* Agrega la clase 'mb-3' para el margen inferior */}
+          <label htmlFor="username" className="form-label">Username:</label> {/* Agrega la clase 'form-label' */}
+          <input
+            type="text"
+            id="username"
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3"> {/* Agrega la clase 'mb-3' para el margen inferior */}
+          <label htmlFor="password" className="form-label">Password:</label> {/* Agrega la clase 'form-label' */}
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary">Login</button> {/* Agrega las clases 'btn' y 'btn-primary' */}
+        </div>
+        {error && <p className="error mt-3">{error}</p>} {/* Agrega la clase 'mt-3' para el margen superior */}
+      </form>
+    </div>
   );
 }
 
