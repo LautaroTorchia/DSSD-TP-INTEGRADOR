160dd8dcbaf47c09e8ac76f1269e9442d5ae3944
Juan <juantvicens@gmail.com>
2023-10-05 16:14:41 -0300

    Fixed styles

--- dssd-furniture-frontend/src/pages/_app.js
+++ dssd-furniture-frontend/src/pages/_app.js
@@ -1,10 +1,13 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+import 'bootstrap/dist/css/bootstrap.css'
+// own css files here
+export function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
 
 export default dynamic(() => Promise.resolve(App), {
   ssr: false,
 });
+// add bootstrap css 
