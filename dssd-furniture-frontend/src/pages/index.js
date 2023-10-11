f4e638e10482d75b90b7bb83b31a5964bce7f71f
Juan <juantvicens@gmail.com>
2023-10-11 10:28:21 -0300

    Fixed bonita project, removed ACME org again

--- dssd-furniture-frontend/src/pages/index.js
+++ dssd-furniture-frontend/src/pages/index.js
@@ -9,38 +9,7 @@ export default function Home() {
         <h1 className={styles.title}>Bienvenido a Global Furniture</h1>
         <p className={styles.subtitle}>Descubre nuestras colecciones de muebles sostenibles.</p>
       </header>
-      <main className={styles.mainContent}>
-        <section className={styles.featuredCollections}>
-          <h2>Destacadas</h2>
-          <div className={styles.collectionItem}>
-            <img src="/image1.jpg" alt="Collection 1" className={styles.collectionImage} />
-            <h3>Nombre de la Colección 1</h3>
-            <p>Descripción de la Colección 1.</p>
-            <button className={styles.viewButton}>Ver Detalles</button>
-          </div>
-          <div className={styles.collectionItem}>
-            <img src="/image2.jpg" alt="Collection 2" className={styles.collectionImage} />
-            <h3>Nombre de la Colección 2</h3>
-            <p>Descripción de la Colección 2.</p>
-            <button className={styles.viewButton}>Ver Detalles</button>
-          </div>
-          {/* Add more featured collections here */}
-        </section>
-        <section className={styles.latestNews}>
-          <h2>Últimas Noticias</h2>
-          <div className={styles.newsItem}>
-            <h3>Título de la Noticia 1</h3>
-            <p>Descripción de la Noticia 1.</p>
-            <button className={styles.readMoreButton}>Leer Más</button>
-          </div>
-          <div className={styles.newsItem}>
-            <h3>Título de la Noticia 2</h3>
-            <p>Descripción de la Noticia 2.</p>
-            <button className={styles.readMoreButton}>Leer Más</button>
-          </div>
-          {/* Add more latest news items here */}
-        </section>
-      </main>
+
       <footer className={styles.footer}>
         {/* Add footer content here */}
       </footer>
