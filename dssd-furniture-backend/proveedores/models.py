7ec56209458fcd1fdf2dfa861169173d5ec27b41
Lautaro Torchia <ltorchia@novakorp.io>
2023-10-16 16:02:39 -0300

    new proveedores app

---
+++ dssd-furniture-backend/proveedores/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Actor(models.Model):
+    TIPO_CHOICES = [
+        ('Proveedor', 'Proveedor'),
+        ('Productor', 'Productor'),
+        ('Reciclador', 'Reciclador'),
+    ]
+    nombre = models.CharField(max_length=100)
+    tipo_actor = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    ubicacion = models.CharField(max_length=100)
+    
+    class Meta:
+        unique_together = ('nombre','tipo_actor')
+
+    def __str__(self):
+        return self.nombre
+
+class Material(models.Model):
+    nombre = models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+class ActorMaterial(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    cantidad_disponible = models.PositiveIntegerField()
+    plazo_entrega_dias = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.actor.nombre} - {self.material.nombre}"
---
+++ dssd-furniture-backend/proveedores/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Actor, Material, ActorMaterial
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+class MaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Material
+        fields = '__all__'
+
+
+class ActorMaterialSerializer(serializers.ModelSerializer):
+    actor_nombre = serializers.CharField(source='actor.nombre', read_only=True)
+    material_nombre = serializers.CharField(source='material.nombre', read_only=True)
+
+    class Meta:
+        model = ActorMaterial
+        fields = ['id', 'actor', 'actor_nombre', 'material', 'material_nombre', 'cantidad_disponible', 'plazo_entrega_dias']
