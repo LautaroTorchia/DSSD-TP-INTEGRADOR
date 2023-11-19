cb53c9594a5db3534c188ba35864612e7b2cb0d8
Juan <juantvicens@gmail.com>
2023-11-19 17:05:06 -0300

    added quarterly collections dashboard graph

--- dssd-furniture-frontend/src/views/dashboard/Dashboard.vue
+++ dssd-furniture-frontend/src/views/dashboard/Dashboard.vue
@@ -1,12 +1,28 @@
-<template>
-  <div>
-    <h1>Dashboard</h1>
-    <QuarterlyCollections />
-    <MaterialsGraph />
-  </div>
-</template>
 
 <script setup>
 import MaterialsGraph from './MaterialsGraph.vue'
 import QuarterlyCollections from './QuarterlyCollections.vue'
 </script>
+
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <QuarterlyCollections />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <MaterialsGraph />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
--- dssd-furniture-frontend/src/views/dashboard/QuarterlyCollections.vue
+++ dssd-furniture-frontend/src/views/dashboard/QuarterlyCollections.vue
@@ -1,22 +1,56 @@
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useCollectionsStore } from '@/stores';
+const collectionStore = useCollectionsStore();
+const { collections } = storeToRefs(collectionStore);
+const quarterCollections = ref([]);
+
+const selectedQuarter = ref('');
+
+const currentYear = new Date().getFullYear();
+
+const generateQuarters = (startYear, endYear) => {
+  const quarters = [];
+  for (let i = startYear; i <= endYear; i++) {
+    for (let j = 1; j <= 4; j++) {
+      quarters.push(`Q${j} ${i}`);
+    }
+  }
+  return quarters;
+};
+
+const quarters = generateQuarters(currentYear - 2, currentYear + 2);
+
+onMounted(async () => {
+  await collectionStore.getAll()
+  collections.value.forEach(async (collection) => {
+    const launchDate = new Date(collection.estimated_launch_date)
+    collection.quarter = `Q${Math.floor((launchDate.getMonth() + 1) / 3) + 1} ${launchDate.getFullYear()}`;
+  })
+})
+
+watch(selectedQuarter, (newQuarter) => {
+  quarterCollections.value = collections.value.filter((collection) => collection.quarter == newQuarter);
+});
+</script>
+
 <template>
-  <div>
-    <h2>Collections Happening This Quarter: {{ quarter }}</h2>
-        {{ collectionsThisQuarter }} - 
+  <div class="container text-center">
+    <h1 class="text-center" style="font-size: 20px;">Seleccionar cuatrimestre</h1>
+    <div class="d-flex justify-content-center">
+      <select v-model="selectedQuarter" class="my-4 form-select" style="font-size: 16px;">
+        <option v-for="quarter in quarters" :key="quarter" :value="quarter">{{ quarter }}</option>
+      </select>
+    </div>
+    <h2 style="font-size: 18px;">Cantidad de colecciones para el cuatrimestre {{ selectedQuarter }}: {{
+      quarterCollections.length }}</h2>
+    <div style="height: 100px; overflow-y: auto;">
+      <ul class="list-group">
+        <li v-for="collection in quarterCollections" :key="collection.id" class="list-group-item"
+          style="font-size: 14px;">{{ collection.name }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script setup>
-import { useCollectionsStore } from '@/stores'
-import { storeToRefs } from 'pinia';
-import { onMounted, ref } from 'vue';
-const collectionsStore = useCollectionsStore()
-const { collections } = storeToRefs(collectionsStore)
-const collectionsThisQuarter = ref([])
-const quarter = Math.floor((new Date().getMonth() / 3) + 1)
-
-onMounted(async () => {
-    await collectionsStore.getAll()
-    
-})
-
-</script>
