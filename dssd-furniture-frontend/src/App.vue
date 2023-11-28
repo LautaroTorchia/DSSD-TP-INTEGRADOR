9f3ae0dc46992713f743cb9b7457149ea15dda4a
Juan <juantvicens@gmail.com>
2023-11-28 16:01:19 -0300

    Is this the end?

--- dssd-furniture-frontend/src/App.vue
+++ dssd-furniture-frontend/src/App.vue
@@ -1,23 +1,16 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
-import { useAuthStore } from '@/stores'
-const authStore = useAuthStore()
-</script>
-
 <template>
     <div class="app-container bg-light">
-        <nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
-            <div class="navbar-nav">
-                <RouterLink to="/" class="nav-item nav-link">Home</RouterLink>
-                <a @click="authStore.logout()" class="nav-item nav-link">Logout</a>
-            </div>
-        </nav>
+        <Navbar/>
         <div class="container pt-4 pb-4">
             <RouterView />
         </div>
     </div>
 </template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+import Navbar from '@/components/NavBar.vue'
+</script>
 
 <style>
 @import '@/assets/base.css'
---
+++ dssd-furniture-frontend/src/components/NavBar.vue
@@ -0,0 +1,13 @@
+
+<template>
+<nav v-show="authStore.user" class="navbar navbar-expand navbar-dark bg-dark">
+    <div class="navbar-nav">
+        <router-link to="/" class="nav-item nav-link">Home</router-link>
+        <a @click="authStore.logout()" class="nav-item nav-link">Logout</a>
+    </div>
+</nav>    
+</template>
+<script setup>
+import { useAuthStore } from '@/stores'
+const authStore = useAuthStore()
+</script>
